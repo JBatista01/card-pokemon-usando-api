285b931951be3ca70dda3ce249d9db5addbb6e77
JBatista01 <jbatista100silva@gmail.com>
2025-06-03 16:23:30 -0300

    cards

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+function App() {
+  
+  const [post, setPost] = useState([])
+  const qtdPoke = 706
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon?limit='+qtdPoke
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(JSON.parse(JSON.stringify(response.data.results)))
+    });
+
+  }, []);
+
+  console.log(post)
+
+  return (
+    <div className="App">
+      {
+        post.map((e) => {
+
+          const pokeNumber = e.url.split('pokemon')[1].replaceAll('/','')
+          console.log(pokeNumber)
+          const urlFoto = 'https://raw.githubusercontent.com/wellrccity/pokedex-html-js/refs/heads/master/assets/img/pokemons/poke_'+pokeNumber+'.gif'
+          
+          return (
+            <div >
+              <h4>
+                {pokeNumber}
+              </h4>
+              <img src={urlFoto}></img>
+              <musica></musica>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
